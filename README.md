c8c6515d15145054cdbfcc193097536afcd2fba2
Alexander St Laurent <stlaurent.alexander@gmail.com>
2015-07-26 12:59:25 -0400

    overhaul: use highcharts for graphs, url query string for build number

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Visualizing the results of benchmarking the Pyret language. See https://github.com/brownplt/pyret-lang/tree/master/tools/benchmark
+
+Many thanks to http://www.highcharts.com/.
--- visualize.js
+++ visualize.js
@@ -1,28 +1,61 @@
-/*global d3*/ //jslint directive
+function alertError (build) {
+  alert('Error loading build ' + build + '. It may not exist.')
+}
 
-document.write('<h3>',
-  filename.substring(0, filename.length - 4).replace(/\-/g, ' '),
-  '</h3>');
-document.write('See Jenkins summary (if it still exists) ',
-  '<a href="',
-  'http://mainmast.cs.brown.edu/job/pyret-benchmark/', 
-  filename.substring('auto-report-'.length, filename.length - 4), 
-  '">here</a>.');
-document.write('<br />');
-document.write('Download ',
-  '<a href="',
-  '../builds/', filename,
-  '">csv file</a>.');
-document.write('<br />');
-document.write('<br />');
-document.write('<div class="chart-container"></div>');
+function visualize (build, normalized) {
+  var filename = 'auto-report-' + build + '.csv';
+  var jenkinsHref = 'http://mainmast.cs.brown.edu/job/pyret-benchmark/' + build;
+  var csvHref = 'builds/' + filename;
 
-d3.csv('../builds/' + filename, function (error, data) {
+  $('#jenkins-nav').attr('href', jenkinsHref);
+  $('#csv-download').attr('href', csvHref);
 
-  var width_correction = 400;
+  return $.ajax({
+    type: 'GET',
+    url: 'builds/' + filename,
+    dataType: 'text',
+    success: function (data) {
+      makeChart(Papa.parse(data), build, normalized);
+    }
+  });
+}
 
-  data = data.filter(function (datum) { return datum.success === 'true'; });
-  var names = data.map(function (datum) { return datum.name; });
+// indexes into csv arrays
+var NAME = 0;
+var SUCCESS = 1;
+var FUNCTION = 2;
+var HZ = 3;
+var RME = 4;
+var SAMPLES = 5;
+
+var CATEGORY_HEIGHT = 120;
+
+var githubFilePrefix = 'https://github.com/brownplt/pyret-lang/tree/master'
++ '/tools/benchmark/auto-report-programs/';
+
+
+function get_hz (datum) {
+  return datum[HZ];
+}
+
+function formatHzValue (hz) {
+  return Math.round(parseFloat(hz) * 1000) / 1000;
+}
+
+function formatPercent (numer, denom) {
+  return Math.round((numer / denom) * 10000) / 100;
+}
+
+function makeChart (parseResult, build, normalized) {
+  data = parseResult.data;
+
+  data = data.filter(function (datum) {
+    return datum[SUCCESS] === 'true'; 
+  });
+
+  var names = data.map(function (datum) {
+    return datum[NAME];
+  });
 
   if (names.length % 3 !== 0) {
     throw new Error('Corrupted CSV data!');
@@ -36,68 +69,121 @@ d3.csv('../builds/' + filename, function (error, data) {
     i = i + 3;
   }
 
-  /* chart_data will be an array of arrays
-    of the form [[parse_hz, load_hz, eval_hz], [...], ...]
-    where chart_data[i] corresponds to names_set[i]
-    */
-  i = 0;
-  var chart_data = [];
-  var make_name_filter = function (i) {
-    return function (datum) {
-      return datum.name === names_set[i];
-    };
-  };
-  var get_hz = function (datum) {
-    return datum.hz;
-  };
-  while (i < names_set.length) {
-    var splice = data.filter(make_name_filter(i));
-    chart_data[i] = splice.map(get_hz);
-    i = i + 1;
+  var parseData = data.filter(function (datum) {
+    return datum[FUNCTION] === 'parse';
+  }).map(get_hz);
+
+  var loadData = data.filter(function (datum) {
+    return datum[FUNCTION] === 'load';
+  }).map(get_hz);
+
+  var evalData = data.filter(function (datum) {
+    return datum[FUNCTION] === 'eval';
+  }).map(get_hz);
+
+  if (normalized) {
+    var parseGold = parseData[0];
+    var loadGold = loadData[0];
+    var evalGold = evalData[0];
+
+    parseData = parseData.map(function (hz) {
+      return formatPercent(hz, parseGold);
+    });
+    loadData  = loadData.map(function (hz) {
+      return formatPercent(hz, loadGold);
+    });
+    evalData  = evalData.map(function (hz) {
+      return formatPercent(hz, evalGold);
+    });
+  } else {
+    parseData = parseData.map(formatHzValue);
+    loadData = loadData.map(formatHzValue);
+    evalData = evalData.map(formatHzValue);
   }
 
-  /* the performance of the empty program will
-    serve as the "gold standard" against which
-    other programs will be measured
-    */
-  var gold_name = '0_empty.arr';
-  var gold_splice = data.filter(function (datum) { return datum.name === gold_name; });
-  var gold_standard = gold_splice.map(function (datum) { return datum.hz; });
-
-  console.log(chart_data);
-  console.log(gold_standard);
-
-  d3.select('.chart-container')
-    .selectAll('div')
-      .data(names_set)
-    .enter().append('div')
-      .text(function (d) { return d; })
-      .attr('class', 'chart')
-      .attr('id', function (d) { return d; });
-
-  d3.selectAll('.chart')
-    .data(chart_data)
-    .each(function (d) {
-      /* d is of the form
-        [parse_hz, load_hz, eval_hz]
-        */
-      d3.select(this)
-        .selectAll('div')
-        .data(d)
-        .enter().append('div')
-        .style('width',
-          function (dd, ii) {
-            return width_correction * (dd / gold_standard[ii]) + 'px';
-          });
+  $(function () {
+    $('#container').highcharts({
+      chart: {
+        type: 'bar',
+        height: names_set.length * CATEGORY_HEIGHT,
+        marginRight: 100
+      },
+      title: {
+        text: 'Pyret Benchmark'
+      },
+      subtitle: {
+        text: 'Build ' + build + (normalized ? ': Normalized' : ': Raw Hertz')
+      },
+      xAxis: {
+        categories: names_set,
+        title: {
+          text: null
+        },
+        labels: {
+          formatter: function () {
+            var disp = this.value;
+            return '<a href="' + githubFilePrefix + this.value 
+            + '" target="_blank">' + disp + '</a>';
+          },
+          useHTML: true
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: normalized ? 'percent' : 'Hertz (ops/second)',
+          align: 'high'
+        },
+        labels: {
+          overflow: 'justify'
+        },
+        opposite: true
+      },
+      tooltip: {
+        valueSuffix: normalized ? ' %' : ' hz'
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true
+          }
+        },
+        series: {
+          pointWidth: 20    
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 30,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true,
+        reversed: true
+      },
+      exporting: {
+         enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'Parse',
+        data: parseData,
+        index: 2
+      }, {
+        name: 'Load',
+        data: loadData,
+        index: 1
+      }, {
+        name: 'Eval',
+        data: evalData,
+        index: 0
+      }]
     });
-
-  var labels = ['parse', 'load', 'eval'];
-  d3.selectAll('.chart')
-    .each(function () {
-      d3.select(this)
-        .selectAll('div')
-        .data(labels)
-        .text(function (d) { return d; });
-    });
-
 });
+
+}
