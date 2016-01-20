68dc5dd9d8621290ebe7c2969079fd54d4021d24
Alexander St Laurent <awstlaur@cs.brown.edu>
2016-01-20 15:13:10 -0500

    jshint-ing

--- visualize.js
+++ visualize.js
@@ -1,3 +1,5 @@
+/* globals alert, $, Papa, Highcharts */
+
 // indexes into csv arrays
 var NAME = 0;
 var SUCCESS = 1;
@@ -7,30 +9,33 @@ var RME = 4;
 var SAMPLES = 5;
 
 var CATEGORY_HEIGHT = 120;
+var MARGIN_RIGHT = 100;
 
 var RANGE_REGEX = /(\d+):(\d+)/;
 
 var symLinks = {
   'ast.arr': '../../../src/arr/trove/ast.arr',
   'anf-loop-compiler.arr': '../../../src/arr/compiler/anf-loop-compiler.arr'
-}
+};
 
-var githubFilePrefix = 'https://github.com/brownplt/pyret-lang/tree/master'
-+ '/tools/benchmark/auto-report-programs/';
+var githubFilePrefix = 'https://github.com/brownplt/pyret-lang/tree/master' + 
+  '/tools/benchmark/auto-report-programs/';
 
-var rawGithubFilePrefix = 'https://raw.githubusercontent.com/brownplt/'
-+ 'pyret-lang/master/tools/benchmark/auto-report-programs/';
+var rawGithubFilePrefix = 'https://raw.githubusercontent.com/brownplt/' + 
+  'pyret-lang/master/tools/benchmark/auto-report-programs/';
 
 
 function alertError (build) {
-  alert('Error loading build ' + build
-    + '. It may not exist, or it hasn\'t synced over. Check the Jenkins server for build errors!');
+  alert('Error loading build ' + build +
+    '. It may not exist, or it hasn\'t synced over. Check the Jenkins server for build errors!');
 }
 
 function visualize (build, normalized, sortByFileSize) {
   var matchRange = build.match(RANGE_REGEX);
-
   var getErrorPlaceHolder = $.Deferred();
+
+  var jenkinsHref;
+  var csvHref;
 
   if(matchRange) {
     var build0 = parseInt(matchRange[1]);
@@ -39,8 +44,8 @@ function visualize (build, normalized, sortByFileSize) {
 
     var filename0 = 'auto-report-' + build0 + '.csv';
     var filename1 = 'auto-report-' + build1 + '.csv';
-    var jenkinsHref = 'http://mainmast.cs.brown.edu/job/pyret-benchmark/' + build0;
-    var csvHref = 'builds/' + filename0;
+    jenkinsHref = 'http://mainmast.cs.brown.edu/job/pyret-benchmark/' + build0;
+    csvHref = 'builds/' + filename0;
 
     $('#jenkins-nav').attr('href', jenkinsHref);
     $('#csv-download').attr('href', csvHref);
@@ -65,8 +70,8 @@ function visualize (build, normalized, sortByFileSize) {
     }); 
   } else {
     var filename = 'auto-report-' + build + '.csv';
-    var jenkinsHref = 'http://mainmast.cs.brown.edu/job/pyret-benchmark/' + build;
-    var csvHref = 'builds/' + filename;
+    jenkinsHref = 'http://mainmast.cs.brown.edu/job/pyret-benchmark/' + build;
+    csvHref = 'builds/' + filename;
 
     $('#jenkins-nav').attr('href', jenkinsHref);
     $('#csv-download').attr('href', csvHref);
@@ -133,7 +138,7 @@ function getProgramSize (name, updateThisHash) {
       updateThisHash[name] = response.length;
     },
     error: function (response) {
-      console.error('Could not load ' + name + '; setting "length" to Infinity.');
+      //console.error('Could not load ' + name + '; setting "length" to Infinity.');
       updateThisHash[name] = Number.POSITIVE_INFINITY;
     }
   });
@@ -143,7 +148,8 @@ function getProgramSize (name, updateThisHash) {
 // in place
 function sortData (sizeOf, names, parseData, loadData, evalData) {
   var bundled = [];
-  for (var i = names.length - 1; i >= 0; i--) {
+  var i;
+  for (i = names.length - 1; i >= 0; i--) {
     bundled[i] = {
       'name': names[i],
       'size': sizeOf[names[i]],
@@ -151,7 +157,7 @@ function sortData (sizeOf, names, parseData, loadData, evalData) {
       'load': loadData[i],
       'eval': evalData[i]
     };
-  };
+  }
 
   bundled = bundled.sort(function (a, b) {
     if (a.size < b.size) {
@@ -163,12 +169,12 @@ function sortData (sizeOf, names, parseData, loadData, evalData) {
     return 0;
   });
   
-  for (var i = bundled.length - 1; i >= 0; i--) {
+  for (i = bundled.length - 1; i >= 0; i--) {
     parseData[i] = bundled[i].parse;
     loadData[i] = bundled[i].load;
     evalData[i] = bundled[i].eval;
     names[i] = bundled[i].name;
-  };
+  }
 }
 
 function get_hz (datum) {
@@ -184,7 +190,7 @@ function formatPercent (numer, denom) {
 }
 
 function makeChart (csvParsed, build, normalized, sortByFileSize, fromSource, filename) {
-  data = csvParsed.data;
+  var data = csvParsed.data;
 
   data = data.filter(function (datum) {
     return datum[SUCCESS] === 'true'; 
@@ -241,111 +247,51 @@ function makeChart (csvParsed, build, normalized, sortByFileSize, fromSource, fi
   if (sortByFileSize) {
     getAllProgramSizes(names_set).done(function (response) {
       sortData(response, names_set, parseData, loadData, evalData, 'size');
-      showChart();
     });
-  } else {
-    showChart();
   }
 
-  function showChart () {
-    $(function () {
-      $('#container').highcharts({
-        chart: {
-          type: 'bar',
-          height: names_set.length * CATEGORY_HEIGHT,
-          marginRight: 100
-        },
-        title: {
-          text: 'Pyret Benchmark'
-        },
-        subtitle: {
-          text: fromSource ? 
-          'Local file: ' + filename
-          : 'Build ' + build + (normalized ? ': Normalized' : ': Raw Hertz')
-        },
-        xAxis: {
-          categories: names_set,
-          title: {
-            text: null
-          },
-          labels: {
-            formatter: function () {
-              var disp = this.value;
-              return '<a href="' + githubFilePrefix + this.value 
-              + '" target="_blank">' + disp + '</a>';
-            },
-            useHTML: true
-          }
-        },
-        yAxis: {
-          min: 0,
-          title: {
-            text: normalized ? 'percent' : 'Hertz (ops/second)',
-            align: 'high'
-          },
-          labels: {
-            overflow: 'justify'
-          },
-          opposite: true
-        },
-        tooltip: {
-          valueSuffix: normalized ? ' %' : ' hz'
-        },
-        plotOptions: {
-          bar: {
-            dataLabels: {
-              enabled: true
-            }
-          },
-          series: {
-            pointWidth: 20    
-          }
-        },
-        legend: {
-          layout: 'vertical',
-          align: 'left',
-          verticalAlign: 'top',
-          x: 30,
-          y: 80,
-          floating: true,
-          borderWidth: 1,
-          backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-          shadow: true,
-          reversed: true
-        },
-        exporting: {
-         enabled: true
-       },
-       credits: {
-        enabled: true
-        },
-        series: [{
-          name: 'Parse',
-          data: parseData,
-          index: 2
-        }, {
-          name: 'Load',
-          data: loadData,
-          index: 1
-        }, {
-          name: 'Eval',
-          data: evalData,
-          index: 0
-        }]
-      });
-    });
-  }
+  var config = {
+    series: [
+      {
+        name: 'Parse',
+        data: parseData,
+        index: 2
+      }, {
+        name: 'Load',
+        data: loadData,
+        index: 1
+      }, {
+        name: 'Eval',
+        data: evalData,
+        index: 0
+      }
+    ],
+    names: names_set,
+    heightFactor: 1,
+    files: [filename],
+    subtitle: fromSource ?
+            'File: ' + filename :
+            'Build ' + build + (normalized ? ': Normalized' : ': Raw Hertz'),
+    xAxisText: '',
+    yAxisText: normalized ? 'percent' : 'Hertz (ops/second)'
+  };
+
+  showChart(config);
 }
 
 function getRangeSliceByName (rangeParsed, index, name) {
   return rangeParsed.map(function (buildData) {
-    // console.log(buildData)
-    return buildData.filter(function (datum) {
-      return datum[index] === name; }) })
+    return buildData.filter(
+      function (datum) {
+        return datum[index] === name;
+      }); 
+  });
 }
 
 function getFileViewOfSlice (slice, fileIndex) {
-  return slice.map(function (sliceData) {return sliceData[fileIndex];})
+  return slice.map(function (sliceData) {
+    return sliceData[fileIndex];
+  });
 }
 
 function makeDiffChart (csv0, csv1, filename0, filename1) {
@@ -402,121 +348,49 @@ function makeDiffChart (csv0, csv1, filename0, filename1) {
 
   function filenameFormat (fn) {
     // remove .csv suffix
-    return fn.slice(0, fn.length - 4)
+    return fn.slice(0, fn.length - 4);
   }
 
   var fn0 = filenameFormat(filename0);
   var fn1 = filenameFormat(filename1);
 
+  var config = {
+    series: [
+      {
+        name: 'Eval ' + fn1,
+        data: eval1,
+      },
+      {
+        name: 'Eval ' + fn0,
+        data: eval0,
+      },
+      {
+        name: 'Load ' + fn1,
+        data: load1,
+      },
+      {
+        name: 'Load ' + fn0,
+        data: load0,
+      },
+      {
+        name: 'Parse ' + fn1,
+        data: parse1,
+      },
+      {
+        name: 'Parse ' + fn0,
+        data: parse0,
+      }
+    ],
+    names: names_set,
+    heightFactor: 2.5,
+    files: [filename0, filename1],
+    subtitle: 'Files: ' + filename0 + ' & ' + filename1,
+    xAxisText: '',
+    yAxisText: 'Hertz (ops/second)'
+  };
 
 
-  showChart();
-
-  function showChart () {
-      $(function () {
-        $('#container').highcharts({
-          chart: {
-            type: 'bar',
-            height: names_set.length * CATEGORY_HEIGHT * 2.5,
-            marginRight: 100
-          },
-          title: {
-            text: 'Pyret Benchmark'
-          },
-          subtitle: {
-            text: 'Files: ' + filename0 + ' & ' + filename1
-          },
-          xAxis: {
-            categories: names_set,
-            title: {
-              text: 'x axis'
-            },
-            labels: {
-              formatter: function () {
-                var disp = this.value;
-                return '<a href="' + githubFilePrefix + this.value 
-                + '" target="_blank">' + disp + '</a>';
-              },
-              useHTML: true
-            }
-          },
-          yAxis: {
-            min: 0,
-            title: {
-              text: 'Hertz (ops/second)',
-              align: 'high'
-            },
-            labels: {
-              overflow: 'justify'
-            },
-            opposite: true
-          },
-          tooltip: {
-            valueSuffix: ' hz'
-          },
-          plotOptions: {
-            bar: {
-              dataLabels: {
-                enabled: true
-              }
-            },
-            series: {
-              pointWidth: 20    
-            }
-          },
-          legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            x: 30,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true,
-            reversed: true
-          },
-          exporting: {
-           enabled: true
-         },
-         credits: {
-          enabled: true
-          },
-          series: [
-            {
-              name: 'Eval ' + fn1,
-              data: eval1,
-              // index: 0
-            },
-            {
-              name: 'Eval ' + fn0,
-              data: eval0,
-              // index: 1
-            },
-            {
-              name: 'Load ' + fn1,
-              data: load1,
-              // index: 0
-            },
-            {
-              name: 'Load ' + fn0,
-              data: load0,
-              // index: 1
-            },
-            {
-              name: 'Parse ' + fn1,
-              data: parse1,
-              // index: 0
-            },
-            {
-              name: 'Parse ' + fn0,
-              data: parse0,
-              // index: 1
-          }
-          ]
-        });
-      });
-  }
+  showChart(config);
 }
 
 function makeRangeChart (rangeParsed, start, end, normalized, sortByFileSize) {
@@ -578,4 +452,79 @@ function makeRangeChart (rangeParsed, start, end, normalized, sortByFileSize) {
           }]
         });
   });
+}
+
+function showChart (config) {
+  $(function () {
+    $('#container').highcharts({
+      chart: {
+        type: 'bar',
+        height: config.names.length * CATEGORY_HEIGHT * config.heightFactor,
+        marginRight: MARGIN_RIGHT
+      },
+      title: {
+        text: 'Pyret Benchmark'
+      },
+      subtitle: {
+        text: config.subtitle
+      },
+      xAxis: {
+        categories: config.names,
+        title: {
+          text: config.xAxisText
+        },
+        labels: {
+          formatter: function () {
+            var disp = this.value;
+            return '<a href="' + githubFilePrefix + this.value +
+              '" target="_blank">' + disp + '</a>';
+          },
+          useHTML: true
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: config.yAxisText,
+          align: 'high'
+        },
+        labels: {
+          overflow: 'justify'
+        },
+        opposite: true
+      },
+      tooltip: {
+        valueSuffix: ' hz'
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true
+          }
+        },
+        series: {
+          pointWidth: 20    
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 30,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true,
+        reversed: true
+      },
+      exporting: {
+       enabled: true
+     },
+     credits: {
+      enabled: true
+    },
+    series: config.series
+  });
+});
 }
